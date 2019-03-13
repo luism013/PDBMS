938b3de80ba42f28d6136b311f06d58d5e5bf2c8
lm_013 <luis.rivera93@upr.edu>
2019-03-13 07:44:08 -0700

    removed "fuck" from print. Added reserved words (WIP)

--- PDBMSMain.py
+++ PDBMSMain.py
@@ -3,5 +3,28 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 
-
-print("Fuck")
+# Reserved Words
+reserved = {
+    'from': 'FROM',
+    'create': 'CREATE',
+    'insert': 'INSERT',
+    'add': 'ADD',
+    'delete': 'DELETE',
+    'get': 'GET',
+    'display': 'DISPLAY',
+    'as': 'AS',
+    'join': 'JOIN',
+    'where': 'WHERE',
+    'update': 'UPDATE',
+    'serial': 'SERIAL',
+    'distinct': 'DISTINCT',
+    'foreign': 'FOREIGN',
+    'key': 'KEY',
+    'cartesian': 'CARTESIAN',
+    'product': 'PRODUCT',
+    'selection': 'SELECTION',
+    'projection': 'PROJECTION',
+    'union': 'UNION',
+    'intersection': 'INTERSECTION',
+    'difference': 'DIFFERENCE'
+}
